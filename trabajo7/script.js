be0f5bb22ef88f7abffa9ecc2ea99a3f30db4b8f
reyna cruz <janeskycruz4@gmail.com>
2023-09-04 16:02:09 -0500

    css

---
+++ trabajo7/script.js
@@ -0,0 +1,80 @@
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("wordpress");
+        habilidades[4].classList.add("drupal");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyect");
+    }
+}
+
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
+
+//<!DOCTYPE html>
+{/* <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Twitter</title>
+    <link rel="stylesheet" href="ejercicio11.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    
+</head>
+</header>
+
+    <body class="grid-container">
+        <header class="header">
+        <i class="fa-solid fa-magnifying-glass"></i>
+        HEADER 
+        </header>
+        <nav class="navbar">
+        <i class="fa-brands fa-twitter"></i>
+        <i class="fa-solid fa-house"></i>
+        NAVBAR
+        </nav>
+        <aside class="sidebar">
+            <i class="fa-brands fa-google"></i>
+            SIDEBAR
+        </aside>
+        <article class="main">
+            MAIN
+        </article>
+        <footer class="footer">
+            FOOTER
+        </footer>
+    </body>
+</html>  
+ */}
+
+ //
